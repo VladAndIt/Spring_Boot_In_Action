22cc06001835c33edbd347aa967ff0bf2c5e9e00
Vlad <trrunovva@gmail.com>
2020-08-21 11:53:21 +0300

    Spring boot in Action Origin

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "<h1>root server</h1>";
+	}
+	@RequestMapping("/var/")
+	String var() {
+		return "Var!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
